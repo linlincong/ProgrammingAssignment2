1e002938aa0f28bff54a24eb493cf0b738fa25d8
Linlin Cong <LinlinCong19@gmail.com>
2016-03-07 21:27:05 -0600

    update codes

--- cachematrix.R
+++ cachematrix.R
@@ -2,11 +2,10 @@
 ### These two function were used together to save the matrix and inverse Matrix into cache, 
 ### so that you donot need to calculate the inverse of a matrix repeatly if you need to use it many times. 
 ###
-### Normally for the first time, makeCacheMatrix were used first to creat a special matrix that can cache its invers matrix.
+### Normally, makeCacheMatrix were used first to creat a special matrix that can cache its invers matrix.
 ### 
-### then when you need the inverse matrix of some matrix, just pass the matrix and the function name (you defined when call makeCacheMatrix function) 
-### to cacheSolve function, it will return the inverse matrix with a message "getting cached data" if the inverse matrix for the same matrix is save 
-### in the cache, otherwise it will calcalte the inverse matrix and update the new matrix and inverseve matrix into cache.
+### then when you need to pass the special matrix to cacheSolve function, it will return the inverse matrix with a message "getting cached data" if the inverse matrix has already been saved 
+### in the cache, otherwise it will calcalte the inverse matrix and update the inverseve matrix into cache.
 
 
 ## makeCacheMatrix function 
@@ -34,30 +33,24 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-
-
 ## cacheSolve function: 
-### 2 input arguments:  
-####    "x" is the matrix used to calculate its inverse matrix with default class matrix,
-####    "func" is the func used before to cach its inverse matrix,
-###  if matrix x and the matrix saved in the cache are the same, and the inverse valued saved in cache is not NULL, then this function will retrun the saved inverse matrix.
-###  otherwise, the inverse matrix will be calculated according to x, and new matrix x and the new inverse matrix will be updated in cache.
+### 1 input argument:  
+####    "x" is the specail matrix created by makeCacheMatrix function before.
+###  if the inverse valued saved in cache is not NULL, then this function will retrun the saved inverse matrix.
+###  otherwise, the inverse matrix will be calculated according to x, and the new inverse matrix will be updated in cache.
 
 
-cacheSolve <- function(x=matrix(), func, ...) {
+cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-
-    data<-func$get()
-    if (identical(data,x)){
-      Inv <- func$getInv()
-      if(!is.null(Inv)) {
-        message("getting cached data")
-        return(Inv)
-      }
-    }
-    Inv <- solve(x, ...)
-    func$set(x)
-    func$setInv(Inv)
-    Inv
+  Inv <- x$getInv()
+  if(!is.null(Inv)) {
+    message("getting cached data")
+    return(Inv)
+  }
+  
+  data<-x$get()
+  Inv <- solve(data, ...)
+  x$setInv(Inv)
+  Inv
   
 }
